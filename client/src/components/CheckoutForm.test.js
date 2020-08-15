d07319367d2144d01d7e1df942ab28303543f393
alexandre steinhauslin <a.steinhauslin@gmail.com>
2020-08-14 18:36:48 -0700

    final save

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -5,9 +5,9 @@ import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("CheckoutForm renders without crashing", () => {
-    render(<CheckoutForm />);
-});
+// test("CheckoutForm renders without crashing", () => {
+//     render(<CheckoutForm />);
+// });
 
 test("form header renders", () => {
     const { getByText } = render(<CheckoutForm />);
@@ -16,10 +16,7 @@ test("form header renders", () => {
 
 test("form shows success message on submit with form details", () => {
      const { getByRole, getByText } = render(<CheckoutForm />)
-//   const name = container.querySelector('input[name="firstName"]')
 
-//     fireEvent.change(name, {target: {value: "Fred" }});
-//     expect(name.value).toEqual("Fred");
     fireEvent.click(getByRole('button', {name: /checkout/i}));
     expect(getByText("Your new green friends will be shipped to:")).not.toBeNull();
 
